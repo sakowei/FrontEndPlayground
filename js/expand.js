0deae755d5bdc1973cec2fd7f13948123bc6234c
weirick <rcwei44@qq.com>
2022-08-07 16:03:58 +0800

    [feat] 饼图 计数器 展开卡片

---
+++ js/expand.js
@@ -0,0 +1,21 @@
+$('.card').on('click', function (e) {
+  let card = $(e.currentTarget);
+  let oftTop = $(card).offset().top - $(window).scrollTop();
+
+  $(card).css('--offset-top', oftTop * -1 + 'px');
+  $(card).toggleClass('active');
+
+  let ratio = 480 / 420;
+  let height = $(window).height;
+  height -= $(card).find('img').outerHeight() * ratio;
+  height -= $(card).find('h4').outerHeight() * ratio;
+  height /= ratio;
+
+  $(card).find('.content').css('height', height);
+
+  if ($(card).hasClass('active')) {
+    $('body').addClass('noscroll');
+  } else {
+    $('body').removeClass('noscroll');
+  }
+});
