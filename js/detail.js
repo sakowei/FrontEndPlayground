b20b682df00dcd921d21060a80fb39d6df6de606
weirick <rcwei44@qq.com>
2022-08-12 17:37:08 +0800

    【添加】手风琴图片、元素周期表、详情卡片

---
+++ js/detail.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper('.slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {invert: false},
+
+  pagination: {
+    el: '.slider-pagination',
+    clickable: true
+  }
+});
